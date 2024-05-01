cb35d96af5dbdc1b97da3511773c9a0ffc9ca2df
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2024-05-01 21:22:48 +0200

    REAFACTOR: Trying another version of TDD

--- src/main/java/com/adriendcv/exercism/wordCount/WordCount.java
+++ src/main/java/com/adriendcv/exercism/wordCount/WordCount.java
@@ -1,14 +1,17 @@
 package com.adriendcv.exercism.wordCount;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class WordCount {
 
 
-    public Map<String, Integer> phrase(String phrase) {
+    public Map<String, Integer> phrase(String string) {
         Map<String, Integer> wordsCount = new HashMap<>();
-        String[] words = phrase.split(" ");
+        String[] words = string.split(" ");
         for (int idx = 0; idx < words.length; idx++) {
             if (wordsCount.containsKey(words[idx])) {
                 wordsCount.put(words[idx], wordsCount.get(words[idx]) + 1);
--- src/test/java/com/adriendcv/exercism/wordCount/WordCountTests.java
+++ src/test/java/com/adriendcv/exercism/wordCount/WordCountTests.java
@@ -1,21 +1,14 @@
 package com.adriendcv.exercism.wordCount;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCountTests {
-    
+
     Map<String, Integer> actualWordCount;
     Map<String, Integer> expectedWordCount;
     WordCount wordCount;
@@ -28,34 +21,47 @@ public class WordCountTests {
     }
 
     @Test
-    void shouldReturnAMapOfOneWordAndItsCounterEqualToOne() {
+    void shouldReturnAMapContainingASingleKeyAndAItsValue() {
         actualWordCount = wordCount.phrase("test");
 
+        Assertions.assertEquals(false, actualWordCount.isEmpty());
         Assertions.assertEquals(1, actualWordCount.size());
-        Assertions.assertEquals(1, actualWordCount.get("test"));
-        Assertions.assertEquals(true, actualWordCount.containsKey("test"));
+        Assertions.assertEquals(1, actualWordCount.keySet().size());
+        Assertions.assertEquals(1, actualWordCount.values().size());
     }
 
     @Test
-    void shouldReturnAMapOfAnyWordOfAPhraseAndTheirCountersEqualToOne() {
+    void shouldReturnAMapContainingASingleKeyAndAItsValueEqualToOne() {
+        actualWordCount = wordCount.phrase("test");
+
+        Assertions.assertEquals(1, actualWordCount.get("test"));
+    }
+
+    @Test
+    void shouldReturnAMapContainingASingleKeyGivenAsAParameterAndAItsValueEqualToOne() {
+        actualWordCount = wordCount.phrase("test");
+
+        Assertions.assertEquals(true, actualWordCount.containsKey("test"));
+        Assertions.assertEquals(1, actualWordCount.get("test"));
+    }
+
+    @Test
+    void shouldReturnAMapContainingSeveralKeysGivenAsAStringInParameterAndATheirValuesEqualToOne() {
         actualWordCount = wordCount.phrase("phrase de test");
 
         Assertions.assertEquals(3, actualWordCount.size());
         Assertions.assertEquals(1, actualWordCount.get("phrase"));
         Assertions.assertEquals(1, actualWordCount.get("de"));
         Assertions.assertEquals(1, actualWordCount.get("test"));
-        Assertions.assertEquals(true, actualWordCount.containsKey("phrase"));
-        Assertions.assertEquals(true, actualWordCount.containsKey("de"));
-        Assertions.assertEquals(true, actualWordCount.containsKey("test"));
     }
 
     @Test
-    void shouldIncrementCounterIfAWordIsPresentSeveralTimes() {
-        actualWordCount = wordCount.phrase("Bonjour je suis Adrien je suis en BUT Informatique");
+    void shouldReturnAMapContainingSeveralKeysGivenAsAStringInParameterAndTheirValuesEqualsToTheNumberOfAppearanceOfTheRelatedKey() {
+        actualWordCount = wordCount.phrase("Bonjour je suis Adrien et je suis en BUT Informatique");
 
-        Assertions.assertEquals(1, actualWordCount.get("Bonjour"));
         Assertions.assertEquals(2, actualWordCount.get("je"));
         Assertions.assertEquals(2, actualWordCount.get("suis"));
+        Assertions.assertEquals(1, actualWordCount.get("Adrien"));
     }
 
 }
