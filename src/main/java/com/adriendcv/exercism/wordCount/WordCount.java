52453a4f32452563eaf81d1640d482e7b3d5a2b3
Adrien DA COSTA VEIGA <adrien.dacostaveiga.etu@univ-lille.fr>
2024-05-01 17:27:19 +0200

    FEAT: add count nbr of appearance of a word in a sentence and tests

--- src/main/java/com/adriendcv/exercism/wordCount/WordCount.java
+++ src/main/java/com/adriendcv/exercism/wordCount/WordCount.java
@@ -1,5 +1,21 @@
 package com.adriendcv.exercism.wordCount;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordCount {
-    
+
+
+    public Map<String, Integer> phrase(String phrase) {
+        Map<String, Integer> wordsCount = new HashMap<>();
+        String[] words = phrase.split(" ");
+        for (int idx = 0; idx < words.length; idx++) {
+            if (wordsCount.containsKey(words[idx])) {
+                wordsCount.put(words[idx], wordsCount.get(words[idx]) + 1);
+            } else {
+                wordsCount.put(words[idx], 1);
+            }
+        }
+        return wordsCount;
+    }
 }
